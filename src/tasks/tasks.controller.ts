3223fd06460ef463a88149ad600b04224e3f44f4
KiriHans <github.mxuxw@slmail.me>
2024-11-11 21:38:18 -0500

    injected Tasks service into Task controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,27 +1,30 @@
 import { Controller, Delete, Get, Patch, Post, Query } from '@nestjs/common';
+import { TasksService } from './tasks.service';
 
 @Controller('tasks')
 export class TasksController {
+  constructor(private readonly tasksService: TasksService) {}
+
   // GET tasks
   @Get()
   findAll(@Query('sort') sort: 'asc' | 'desc' = 'desc'): string {
-    return 'All tasks';
+    return this.tasksService.getTasks();
   }
 
   // POST tasks
   @Post(':id')
   createTask(): string {
-    return 'Task created';
+    return this.tasksService.createTask();
   }
   // PATCH tasks
   @Patch()
   updateTask(): string {
-    return 'Task updated';
+    return this.tasksService.updateTask();
   }
 
   // DELETE tasks
   @Delete()
   deleteTask(): string {
-    return 'Task deleted';
+    return this.tasksService.deleteTask();
   }
 }
--- src/tasks/tasks.service.ts
+++ src/tasks/tasks.service.ts
@@ -4,19 +4,23 @@ import { Injectable } from '@nestjs/common';
 export class TasksService {
   createTask() {
     // Create task database
+    return 'Task created';
   }
 
   deleteTask() {
     // Delete task database
+    return 'Task deleted';
   }
 
   updateTask() {
     // Update task database
+    return 'Task updated';
   }
 
   getTasks() {
     // Get all user tasks
     // Posible to get using filters
     // Or pagination
+    return 'Task found';
   }
 }
