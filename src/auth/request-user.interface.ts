9bbaeb65cc843fe280146898f9df3785996629be
KiriHans <github.mxuxw@slmail.me>
2024-11-18 22:39:12 -0500

    Changed Request import in request-user interface from express to nestjs/common

--- src/auth/request-user.interface.ts
+++ src/auth/request-user.interface.ts
@@ -1,4 +1,4 @@
-import { Request } from 'express';
+import { Request } from '@nestjs/common';
 import { User } from 'src/user/entities/user.entity';
 
 export interface RequestWithUser extends Request {
